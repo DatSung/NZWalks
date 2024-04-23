78dce3f5e7bdc2f6ed4de2c5d0fcee12c5eba1fb
DatSung <datsung.dev@gmail.com>
2024-04-23 16:32:37 +0700

    delete unuse projects

--- NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalks.API/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d5b090bfa2936b1dddfd6b689877b0752ad23f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+994ac267c38449b34df87602ce0e393d416719c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
